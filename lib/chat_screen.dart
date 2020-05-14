0e6178e5f89e40bede4cfb5d8680ec5107f3f556
ocostalucas <olucascosta98@gmail.com>
2020-05-14 20:33:33 -0300

    added list of messages

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -10,36 +10,62 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-
-  void _sendMessage({String text, File imgFile}) async{
+  void _sendMessage({String text, File imgFile}) async {
     Map<String, dynamic> data = {};
 
+    if (imgFile != null) {
+      StorageUploadTask task = FirebaseStorage.instance
+          .ref()
+          .child('images')
+          .child(DateTime.now().millisecondsSinceEpoch.toString())
+          .putFile(imgFile);
 
-    if(imgFile != null){
-      StorageUploadTask task = FirebaseStorage.instance.ref().child('images').child(
-        DateTime.now().millisecondsSinceEpoch.toString()
-        ).putFile(imgFile);
-      
       StorageTaskSnapshot taskSnapshot = await task.onComplete;
       String url = await taskSnapshot.ref.getDownloadURL();
       data['imgUrl'] = url;
     }
 
-    if(text != null) data['text'] = text;
+    if (text != null) data['text'] = text;
 
     Firestore.instance.collection('messages').add(data);
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Olá'),
-      elevation: 0,
+      appBar: AppBar(
+        title: Text('Olá'),
+        elevation: 0,
       ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+            stream: Firestore.instance.collection('messages').snapshots(),
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                case ConnectionState.waiting:
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                default:
+                  List<DocumentSnapshot> documents = 
+                  snapshot.data.documents.reversed.toList();
 
-      body: TextComposer(
-       _sendMessage
+                  return ListView.builder(
+                      itemCount: documents.length,
+                      reverse: true,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text(documents[index].data['text']),
+                        );
+                      });
+              }
+            },
+          )),
+          TextComposer(_sendMessage),
+        ],
       ),
     );
   }
